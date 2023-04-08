b66fbeb497da21c369a138fc1f5e93e90e4e380c
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-04-08 14:13:00 -0600

    Passed and saved

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  first letter is the esame as the last letter and so on throughout the 
+  word until you reach the middle of the word
+
+  iterate from the beginning to the middle of the word and check
+  if each letter matches
+
+  if they match it will return true
+  if they don't match it will return false
 */
 
 /*
-  Add written explanation of your solution here
+  if the value of the word is not a palidrome the code will return false
+  if the value of the word is equal to a palidrome the code will return true
+  it will iterate through half the lenght of the word
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +36,36 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  if (require.main === module) {
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("mom"));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("abba"));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("a"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("hi"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
 }
 
 module.exports = isPalindrome;
